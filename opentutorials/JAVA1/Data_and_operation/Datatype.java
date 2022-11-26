14dacff85dbfd71a719d45b1318c042023ad89bd
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-11-26 17:18:23 +0900

    Data type

---
+++ opentutorials/JAVA1/Data_and_operation/Datatype.java
@@ -0,0 +1,16 @@
+public class Datatype{
+	public static void main(String[] args) {
+    
+		System.out.println(6); // Number
+		System.out.println("six"); // String
+		System.out.println("6"); // String 6
+		
+		System.out.println(6+6); // 12
+		System.out.println("6"+"6"); // 66
+//		System.out.println("6"*"6"); // 문자열끼리 곱셈 불가
+		
+		System.out.println("1111".length()); // 4 : 문자열의 길이
+//		System.out.println(1111.length()); // 숫자의 길이 알려주지 않음
+		
+	}
+}

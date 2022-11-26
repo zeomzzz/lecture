319634b9cee983de9970f98774105d830f3c6014
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-11-26 17:19:24 +0900

    Number operation

---
+++ opentutorials/JAVA1/Data_and_operation/Number.java
@@ -0,0 +1,17 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// Operator
+		System.out.println(6 + 2); // 8
+		System.out.println(6 - 2); // 4
+		System.out.println(6 * 2); // 12
+		System.out.println(6 / 2); // 3
+		
+		System.out.println(Math.PI); // 3.141592653589793
+		System.out.println(Math.floor(Math.PI)); // 3.0 : 소수점 자름(내림)
+		System.out.println(Math.ceil(Math.PI)); // 4.0 : 소수점 올림
+
+	}
+
+}

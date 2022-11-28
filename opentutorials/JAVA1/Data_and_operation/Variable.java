a1eb46489e91106fd5fe0b688adb08be96488021
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-11-28 20:10:02 +0900

    Variable

---
+++ opentutorials/JAVA1/Data_and_operation/Variable.java
@@ -0,0 +1,17 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a = 1; // Number -> integer (정수)
+		System.out.println(a);
+		
+		double b = 1.1; // real number (실수) -> double
+		System.out.println(b);
+		
+		String c = "Hello World";
+		System.out.println(c);
+		
+	}
+
+}

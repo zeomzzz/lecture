fa4ef9172f5f28a080c1cc23d92d8a061c36d8b5
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-29 11:04:05 +0900

    Update java class, instance

--- opentutorials/JAVA1/MyApp/AccountingClassApp.java
+++ opentutorials/JAVA1/MyApp/AccountingClassApp.java
@@ -1,20 +1,9 @@
-
-public class AccountingClassApp {
-
-	public static double valueOfSupply;
-	public static double vatRate;
-	public static double expenseRate;
-
-	public static void main(String[] args) {
-		
-		valueOfSupply = 10000.0;
-		vatRate = 0.1;
-		expenseRate = 0.3;
-		
-		print();
-	}
-
-	public static void print() {
+class Accounting{
+	public double valueOfSupply;
+	public double vatRate;
+	public double expenseRate;
+	
+	public void print() {
 		System.out.println("Value of supply : " + valueOfSupply);
 		System.out.println("VAT : " + getVAT());
 		System.out.println("Total : " + getTotal());
@@ -23,34 +12,50 @@ public class AccountingClassApp {
 		System.out.println("Dividened 1 : " + getDividend1());
 		System.out.println("Dividened 2 : " + getDividend2());
 		System.out.println("Dividened 3 : " + getDividend3());
-	}
-
-	public static double getDividend1() {
-		return getIncome() * 0.5;
-	}
-
-	public static double getDividend2() {
-		return getIncome() * 0.3;
-	}
 	
-	public static double getDividend3() {
-		return getIncome() * 0.2;
-	}
-	
-	public static double getIncome() {
-		return valueOfSupply - getExpense();
+		public double getDividend1() {
+			return getIncome() * 0.5;
+		}
+
+		public double getDividend2() {
+			return getIncome() * 0.3;
+		}
+		
+		public double getDividend3() {
+			return getIncome() * 0.2;
+		}
+		
+		public double getIncome() {
+			return valueOfSupply - getExpense();
+		}
+
+		public double getExpense() {
+			return valueOfSupply * expenseRate;
+		}
+
+		public double getTotal() {
+			return valueOfSupply + getVAT();
+		}
+
+		public double getVAT() {
+			return valueOfSupply * vatRate;
+		}
 	}
 
-	public static double getExpense() {
-		return valueOfSupply * expenseRate;
-	}
 
-	public static double getTotal() {
-		return valueOfSupply + getVAT();
-	}
+public class AccountingClassApp {	
 
-	public static double getVAT() {
-		return valueOfSupply * vatRate;
-	}
+	public static void main(String[] args) {
 
+		// Instance
+		Accounting a1 = new Accounting();
+		a1.valueOfSupply = 10000.0;
+		a1.vatRate = 0.1;
+		a1.expenseRate = 0.3;
+		
+		Accounting a2 = new Accounting();
+		a2.valueOfSupply = 20000.0;
+		a2.vatRate = 0.05;
+		a2.expenseRate = 0.3;
+	}
 }
